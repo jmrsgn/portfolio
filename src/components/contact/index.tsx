31c95de95e08b966227820a691b728dda513d1e6
John Martin Marasigan <marasiganjohnmartin@gmail.com>
2026-08-23 15:32:00 +0800

    🧹 cleanup

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { ItemButton,
-         Container,
-         ContainerInfo,
-         Box,
-         BoxContact,
-         TextCopyright,
-         TextTitle,
-         TextDescription } from './styles/contact';
-
+import {
+  ItemButton,
+  Container,
+  ContainerInfo,
+  Box,
+  BoxContact,
+  TextCopyright,
+  TextTitle,
+  TextDescription,
+} from "./styles/contact";
 
 // Accept props whatever Container accepts
 type ContactProps = React.ComponentProps<typeof Container>;
@@ -21,49 +22,67 @@ type TextTitleProps = React.ComponentProps<typeof TextTitle>;
 type TextDescriptionProps = React.ComponentProps<typeof TextDescription>;
 
 export default function Contact({ children, ...restProps }: ContactProps) {
-    return <Container {...restProps}>{children}</Container>
+  return <Container {...restProps}>{children}</Container>;
 }
 
 /**
  * * Items
  */
 
-Contact.ItemButton = function ContactButton({ children, ...restProps }: ItemButtonProps) {
-    return <ItemButton {...restProps}>{children}</ItemButton>
-}
+Contact.ItemButton = function Component({
+  children,
+  ...restProps
+}: ItemButtonProps) {
+  return <ItemButton {...restProps}>{children}</ItemButton>;
+};
 
 /**
  * * Containers
  */
 
-Contact.ContainerInfo = function ContactContainerInfo({ children, ...restProps }: ContainerInfoProps) {
-    return <ContainerInfo {...restProps}>{children}</ContainerInfo>
-}
+Contact.ContainerInfo = function Component({
+  children,
+  ...restProps
+}: ContainerInfoProps) {
+  return <ContainerInfo {...restProps}>{children}</ContainerInfo>;
+};
 
 /**
  * * Boxes
  */
 
-Contact.Box = function ContactBox({ children, ...restProps }: BoxProps) {
-    return <Box {...restProps}>{children}</Box>
-}
+Contact.Box = function Component({ children, ...restProps }: BoxProps) {
+  return <Box {...restProps}>{children}</Box>;
+};
 
-Contact.BoxContact = function ContactBoxContact({ children, ...restProps }: BoxContactProps) {
-    return <BoxContact {...restProps}>{children}</BoxContact>
-}
+Contact.BoxContact = function Component({
+  children,
+  ...restProps
+}: BoxContactProps) {
+  return <BoxContact {...restProps}>{children}</BoxContact>;
+};
 
 /**
  * * Texts
  */
 
-Contact.TextCopyright = function ContactTextCopyright({ children, ...restProps }: TextCopyrightProps) {
-    return <TextCopyright {...restProps}>{children}</TextCopyright>
-}
+Contact.TextCopyright = function Component({
+  children,
+  ...restProps
+}: TextCopyrightProps) {
+  return <TextCopyright {...restProps}>{children}</TextCopyright>;
+};
 
-Contact.TextTitle = function ContactTextTitle({ children, ...restProps }: TextTitleProps) {
-    return <TextTitle {...restProps}>{children}</TextTitle>
-}
+Contact.TextTitle = function Component({
+  children,
+  ...restProps
+}: TextTitleProps) {
+  return <TextTitle {...restProps}>{children}</TextTitle>;
+};
 
-Contact.TextDescription = function ContactTextDescription({ children, ...restProps }: TextDescriptionProps) {
-    return <TextDescription {...restProps}>{children}</TextDescription>
-}
+Contact.TextDescription = function Component({
+  children,
+  ...restProps
+}: TextDescriptionProps) {
+  return <TextDescription {...restProps}>{children}</TextDescription>;
+};
